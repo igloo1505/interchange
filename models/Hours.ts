87bc0a3432f22e47aa99b7f83ed082da99517a67
Andrew Iglinski <Andrew@igloodevelopment.dev>
2022-12-06 08:09:30 -0600

    model fix

--- models/Hours.ts
+++ models/Hours.ts
@@ -1,68 +1,61 @@
 import { Schema, models, model, Types } from "mongoose";
 
-
-
-
 interface DailyHoursInterface {
-    opening: number,
-    closing: number
+	opening: number;
+	closing: number;
 }
 
 const DailySchema = new Schema({
-  opening: {
-    type: int,
-    required: true,
-  },
-  closing: {
-    type: int,
-    required: true,
-  },
+	opening: {
+		type: Number,
+		required: true,
+	},
+	closing: {
+		type: Number,
+		required: true,
+	},
 });
-
 
 interface HoursInterface {
-    monday: DailyHoursInterface
-    tuesday: DailyHoursInterface
-    wednesday: DailyHoursInterface
-    thursday: DailyHoursInterface
-    friday: DailyHoursInterface
-    saturday: DailyHoursInterface
-    sunday: DailyHoursInterface
+	monday: DailyHoursInterface;
+	tuesday: DailyHoursInterface;
+	wednesday: DailyHoursInterface;
+	thursday: DailyHoursInterface;
+	friday: DailyHoursInterface;
+	saturday: DailyHoursInterface;
+	sunday: DailyHoursInterface;
 }
 
-
 const HoursSchema = new Schema<HoursInterface>({
-  monday: {
-    type: Schema.Types.ObjectId,
-    required: false,
-  },
-  tuesday: {
-    type: Schema.Types.ObjectId,
-    required: false,
-  },
-  wednesday: {
-    type: Schema.Types.ObjectId,
-    required: false,
-  },
-  thursday: {
-    type: Schema.Types.ObjectId,
-    required: false,
-  },
-  friday: {
-    type: Schema.Types.ObjectId,
-    required: false,
-  },
-  saturday: {
-    type: Schema.Types.ObjectId,
-    required: false,
-  },
-  sunday: {
-    type: Schema.Types.ObjectId,
-    required: false,
-  },
+	monday: {
+		type: Schema.Types.ObjectId,
+		required: false,
+	},
+	tuesday: {
+		type: Schema.Types.ObjectId,
+		required: false,
+	},
+	wednesday: {
+		type: Schema.Types.ObjectId,
+		required: false,
+	},
+	thursday: {
+		type: Schema.Types.ObjectId,
+		required: false,
+	},
+	friday: {
+		type: Schema.Types.ObjectId,
+		required: false,
+	},
+	saturday: {
+		type: Schema.Types.ObjectId,
+		required: false,
+	},
+	sunday: {
+		type: Schema.Types.ObjectId,
+		required: false,
+	},
 });
 
-
-
 export default models?.HoursSchema ||
-	model<HoursSchema>("Hours", HoursSchema);
+	model<HoursInterface>("Hours", HoursSchema);
