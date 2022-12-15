88e9dadb73a3623966d74c432299314ea696fd58
Andrew Iglinski <Andrew@igloodevelopment.dev>
2022-12-15 16:22:19 -0600

    lord this is driving me nuts

--- components/feed/Paginate.tsx
+++ components/feed/Paginate.tsx
@@ -38,7 +38,7 @@ interface buttonInterface {
 
 const Paginate = connector(
 	({ feed: { data, page, total, query } }: PaginateProps) => {
-		const [buttons, setButtons] = useState<buttonInterface[]>([]);
+		const [buttons, setButtons] = useState<buttonInterface[]>([1]);
 		const dispatch = useAppDispatch();
 		useEffect(() => {
 			setButtons(getButtons(Math.ceil(total / PER_PAGE), page, total));
